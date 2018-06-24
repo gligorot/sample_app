fb79dc62b739c5eb5b4bd102ae7de98902353732
gligorot <dracevo.kromid@gmail.com>
2018-06-24 19:45:23 +0200

    Finish user edit, update, index and destroy actions/ add admin functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,8 @@ class User < ApplicationRecord
 										uniqueness: { case_sensitive: false }
 
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  #has secure password catches true nils at creation, this is for update (remain same)
 
   # Returns the hash digest of the given string.
   def User.digest(string)
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  test "layout links" do
+  def setup
+    @user = users(:michael)
+  end
+
+  test "not logged in layout links" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
@@ -11,5 +15,19 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 
     get contact_path
     assert_select "title", full_title("Contact")
+  end
+
+  test "logged in layout links" do
+    log_in_as @user
+
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", logout_path
+
   end
 end
